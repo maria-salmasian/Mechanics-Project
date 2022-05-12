8f6477d08b58e2a6f2c938a6294873ffb5f391d8
maria.salmasian <maria.salmasian@disqo.com>
2022-05-12 20:56:13 +0400

    -added base methods

--- Spring.java
+++ Spring.java
@@ -14,4 +14,58 @@ public class Spring {
     //– a body of a specified mass double m oscillates from t = t0 till t = t1 with initial
     //conditions x(t0) = x0 and v(t0) = v0. The coordinate is computed per each double
     //dt time step;
+
+    Integer k_stiffness = 1;
+
+    public Spring(Integer k_stiffness) {
+        this.k_stiffness = k_stiffness;
+    }
+
+    public Spring() {
+    }
+
+    public Integer getK_stiffness() {
+        return k_stiffness;
+    }
+
+    public void setK_stiffness(Integer k_stiffness) {
+        this.k_stiffness = k_stiffness;
+    }
+
+    public Spring inSeries(){
+        return new Spring();
+    }
+
+    public Spring inParallel(){
+        return new Spring();
+    }
+
+
+    public Double[] move(double t, double dt, double x0, double v0) {
+        double e = (Math.pow(x0, 2) * this.k_stiffness)/2 + Math.pow(v0, 2)/2;
+        return coord(e, t, dt);
+    }
+
+    public Double[] move(double t, double dt, double x0) {
+        double e = (Math.pow(x0, 2) / 2);
+        return coord(e, t, dt);
+    }
+
+    public Double[] move(double t0, double tl, double dt, double x0, double v0) {
+        double e = (Math.pow(x0, 2) * this.k_stiffness)/2 + Math.pow(v0, 2)/2;
+        double t = tl - t0;
+        return coord(e, t, dt);
+    }
+
+    public Double[] move(double t0, double tl, double dt, double x0, double v0, double m) {
+        double e = (Math.pow(x0, 2) * this.k_stiffness)/2 + Math.pow(v0, 2)*m/2;
+        double t = tl - t0;
+        return coord(e, t, dt);
+    }
+
+
+
+    private Double[] coord(double e, double t, double dt) {
+        return new Double[0];
+    }
 }
--- SpringArray.java
+++ SpringArray.java
@@ -3,4 +3,13 @@ public class SpringArray {
     //TODO: add equivalentSpring(String springExpr)
 
     //TODO: equivalentSpring(String springExpr, Spring[] springs)
+
+    public Spring equivalentSpring(String springExpr){
+        return new Spring();
+    }
+
+
+    public Spring equivalentSpring(String springExpr, Spring[] springs){
+        return new Spring();
+    }
 }
